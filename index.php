dae15040e7bcbeb07b24acded3f6b5c7ad303f32
Victoria Stauffer <vstauffer21@gmail.com>
2020-05-31 15:48:41 -0400

    server update

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
-// Step 1: Create Database Connection
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = "idm232";
+$host = $_SERVER['HTTP_HOST'];
+if ($host == 'localhost:8888') {
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpass = "root";
+  $dbname = "idm232";
+}
+else {
+  // Remote database credentials
+  $dbhost = "localhost";
+  $dbuser = "vstauffe_idm232";
+  $dbpass = "AHenry108";
+  $dbname = "vstauffe_idm232";
+}
+
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-// Check the connection is good with no errors
 if (mysqli_connect_errno()) {
-  die ("Database connection failed: " .
-    mysqli_connect_error() .
-    " (" . mysqli_connect_errno() . ")"
-  );
-}
+    die("Database connection failed: " .
+      mysqli_connect_error() .
+      " (" . mysqli_connect_errno() . ")"
+    );
+  }
 
 ?>
 
