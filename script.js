e295e6db08df7c78fb37b64fb28796f60a53eada
Victoria Stauffer <vstauffer21@gmail.com>
2020-06-23 16:05:13 -0400

    latest

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-$('#search').keypress(function (e) {
-    var str = $('#search').val();
-    var domain = "http://www.yourdomain.com";
-    var url = domain+"default.aspx?search=" + str;
-    if (e.keyCode == 13) {
-        location.href = url;
+var button = document.getElementsByClassName('filters');
+var clicked = false;
+console.log('script is here');
+
+button.onclick = function(){
+    console.log(click)
+    if(!clicked){
+        (button.style.color) = '#ce897a';
+        clicked = true;
+        console.log("clicked");
     }
-});
+    else{
+        (button.style.color) = '#256d69';
+        clicked = false;
+        console.log(unclicked);
+    }
+}
