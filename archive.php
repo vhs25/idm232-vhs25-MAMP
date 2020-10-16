9d4a4f994b013d55323d502d23b0a969bb07bbbe
Victoria Stauffer <vstauffer21@gmail.com>
2020-10-16 15:38:26 -0400

    update

---
+++ archive.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<link href="style.css" type="text/css" rel="stylesheet">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="top">
+    <div  class="head_logo">
+        <a href="index.php">
+            <img src="med/logo_med.png" alt="painted header reading Victoria Stauffer">
+        </a>
+    </div>
+
+    <div class="nav">
+        <a href="portfolio.php">projects</a>
+        <a href="art_portfolio.php">artwork</a>
+    </div>
+    </div>    
+<main>
+    <div class="all_tiles">
+            <div class="tile">
+                    <img src="tiles/zodiac_tile.png" alt="image of the home screen of my fashion app prototype">
+                    <header>Zodiac Website</header>
+                    <p>View my <a href="zodiac/index.html" target="_blank">Zodiac Website</a>.</p>
+                </div>
+        <div class="tile">
+            <img src="tiles/bow_tile.png" alt="image of the home screen of my fashion app prototype">
+            <header>Bow App Prototype</header>
+            <p>This is the prototype of my fashion app idea named 'Bow,' created for a class app competition (which I won!).</p>
+        </div>
+        <div class="tile">
+            <img src="tiles/ss_tile.png" alt="image of sweat sensor device">
+            <header>Sweat Sensor</header>
+            <p>I built this sweat sensor device with Arduino to help solve the problem that is my eternally sweaty hands.</p>
+        </div>
+        <div class="tile">
+            <img src="tiles/hair_tile.png" alt="stippled painting of short curly hair">
+            <header>ASMRtistry</header>
+            <p>ASMRtistry is a soothing video featuring <a href="https://b7s9.com/" target="_blank">Ben Schenerman's</a> music and a time lapse of our stipple paintings of hair.</p>
+        </div>
+    </div>
+</main>
+</body>
+</html>
